7d3923816546368536653300344a4d09c133c672
hoang dat <nhat67317@gmail.com>
2021-05-28 11:05:23 +0700

    update css, js library

---
+++ js/login.js
@@ -0,0 +1,8 @@
+function myFunction(){
+var email = document.getElementById("email").value;
+var pass = document.getElementById("password").value;
+
+if(email == "" || pass == "") {
+  alert("Vui lòng nhập đầy đủ email và pasword. ");
+}
+}
